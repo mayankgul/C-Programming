14ba619af96e3deec368cb53928d6fb1a24d0ff6
Mayank Gulati <sagax.dg@gmail.com>
2025-09-27 18:31:11 +0530

    initial commit

---
+++ introduction/hello-world.c
@@ -0,0 +1,12 @@
+// program to print "hello world" to the console
+
+// the following statement is a preprocessor directive
+// preprocessor: converts - source code -> expanded source code
+// compiler: converts - expanded source code -> machine code
+#include <stdio.h>
+
+int main()
+{
+    printf("hello world\n");
+    return 0;
+}

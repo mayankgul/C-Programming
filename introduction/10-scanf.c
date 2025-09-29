0e5cb41c201e4b391b0486e45ae7d95121069f20
Mayank Gulati <sagax.dg@gmail.com>
2025-09-29 12:34:51 +0530

    update scanf

--- introduction/10-scanf.c
+++ introduction/10-scanf.c
@@ -3,10 +3,10 @@
 int main()
 {
     int a, b;
-    printf("Enter a: ");
+    printf("Enter a (%p): ", (void*)&a);
     scanf("%d", &a);
 
-    printf("Enter b: ");
+    printf("Enter b (%p): ", (void*)&b);
     scanf("%d", &b);
 
     printf("%d + %d = %d\n", a, b, a + b);
